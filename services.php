43d40c4a5221074c4da68926d36dd10710e6a860
Carlos García Gómez <neorazorx@gmail.com>
2019-11-23 17:16:56 +0100

    record audio and send to servies.php

--- services.php
+++ services.php
@@ -2,8 +2,10 @@
 
 require 'vendor/autoload.php';
 
+var_dump($_POST);
+
 // we load the language model, it would create
 // the $config object for us.
 $detect = LanguageDetector\Detect::initByPath('datafile.php');
 
-echo $detect->detect($_POST['text']);
+echo $detect->detect('Hello, my name is peter.');

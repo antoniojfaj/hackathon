24aeebbc9f92e2aab21fc8065b0c84436b6315ad
Antonio Fajardo <info@antoniofajardo.com>
2019-11-23 14:30:24 +0100

    Inicial

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>REAC</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/reac.css" />
+    </head>
+    <body>
+        <div id="contenedor">
+            <h1>REAC - Estamos para ayudarte</h1>
+            <h2>Selecciona un idioma</h2>
+            <hr/>
+            <div class="selectorIdioma">
+                
+            </div>
+        </div>
+        
+    </body>
+    <script src="js/jQuery.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+</html>

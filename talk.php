68fe8274ecfaed1294da166854bb1419d285dac5
Carlos García Gómez <neorazorx@gmail.com>
2019-11-24 12:14:40 +0100

    refactorizado código

--- talk.php
+++ talk.php
@@ -1,48 +1,8 @@
 <?php
 
-// includes the autoloader for libraries installed with composer
-require __DIR__ . '/vendor/autoload.php';
+require 'vendor/autoload.php';
 putenv('GOOGLE_APPLICATION_CREDENTIALS='.__DIR__.'/hackathon-7659e0d010b7.json');
+require 'lib/translator.php';
 
-// Imports the Cloud Client Library
-use Google\Cloud\TextToSpeech\V1\AudioConfig;
-use Google\Cloud\TextToSpeech\V1\AudioEncoding;
-use Google\Cloud\TextToSpeech\V1\SsmlVoiceGender;
-use Google\Cloud\TextToSpeech\V1\SynthesisInput;
-use Google\Cloud\TextToSpeech\V1\TextToSpeechClient;
-use Google\Cloud\TextToSpeech\V1\VoiceSelectionParams;
-
-// instantiates a client
-$client = new TextToSpeechClient();
-
-// sets text to be synthesised
-$text = isset($_POST['text']) ? $_POST['text'] : 'Notdienste werden empfohlen. In wenigen Minuten werden sie zu Ihrem Standort gehen.';
-$synthesisInputText = (new SynthesisInput())
-    ->setText($text);
-
-// build the voice request, select the language code ("en-US") and the ssml
-// voice gender
-$voice = (new VoiceSelectionParams())
-    ->setLanguageCode('de-DE')
-    ->setSsmlGender(SsmlVoiceGender::FEMALE);
-
-// Effects profile
-$effectsProfileId = "telephony-class-application";
-
-// select the type of audio file you want returned
-$audioConfig = (new AudioConfig())
-    ->setAudioEncoding(AudioEncoding::MP3)
-    ->setEffectsProfileId(array($effectsProfileId));
-
-// perform text-to-speech request on the text input with selected voice
-// parameters and audio file type
-$response = $client->synthesizeSpeech($synthesisInputText, $voice, $audioConfig);
-$audioContent = $response->getAudioContent();
-
-// the response's audioContent is binary
-$fileName = 'output.mp3';
-if(file_exists($fileName)) {
-    unlink($fileName);
-}
-file_put_contents($fileName, $audioContent);
-header('Location: '.$fileName);
+$translator = new translator();
+header('Location: '.$translator->generateAufioFile());
